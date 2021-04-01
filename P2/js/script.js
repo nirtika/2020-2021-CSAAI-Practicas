07c4f3d67eb985af34c4bbbf9ee71c65656ed78b
Nirtika <14869446+nirtika@users.noreply.github.com>
2021-04-01 23:16:10 +0200

    calc_scientifico

--- P2/js/script.js
+++ P2/js/script.js
@@ -7,7 +7,7 @@ const reset = document.getElementById("reset")
 const clear = document.getElementById("clear")
 const punto = document.getElementById("punto")
 const ans = document.getElementById("ans")
-
+const porcentaje = document.getElementById('porcentaje');
 // todos los 'digits' que hay en el html
 let digitos = document.getElementsByClassName("digits")
 //-- array con todos los elementos de la clase operacion
@@ -15,6 +15,9 @@ let operacion = document.getElementsByClassName("operaciones")
 
 var resultado; // valor de resultado
 
+const sin =document.getElementById('sin');
+const cos =document.getElementById('cos');
+const tan =document.getElementById('tan');
 //-- Estados de la calculadora
 const ESTADO = {
     INIT: 0,
@@ -30,6 +33,9 @@ let estado = ESTADO.INIT;
 //-- el array de los digitos
 for (i=0; i<digitos.length; i++){
   digitos[i].onclick = (ev)=> {
+    sin.disabled = false;
+    cos.disabled = false;
+    tan.disabled= false;
     digito(ev.target.value); // ev=evento, ev.target da el valor del boton que ha sido pulsado
   }
 }
@@ -37,6 +43,9 @@ for (i=0; i<digitos.length; i++){
 //-- el array de los operadores: sumar, restar, multiplicar, dividir,
 for (i=0; i<operacion.length; i++){
   operacion[i].onclick = (ev)=> {
+    sin.disabled = false;
+    cos.disabled = false;
+    tan.disabled= false;
     if(estado == ESTADO.OP1){
       display.innerHTML += ev.target.value;
       estado = ESTADO.OPERATION;
@@ -44,7 +53,11 @@ for (i=0; i<operacion.length; i++){
     }
   }
 }
-
+if(display.innerHTML =="" && display_err.innerHTML==""){
+  sin.disabled = true;
+  cos.disabled = true;
+  tan.disabled = true;
+}
 function digito(boton) {
   if(estado == ESTADO.INIT) {
       display.innerHTML = boton;
@@ -56,17 +69,52 @@ function digito(boton) {
         ESTADO.comprobarcoma = false;
       }
     }
-  }
-const porcentaje = document.getElementById('porcentaje')
+    
+}
 porcentaje.onclick =() =>{
     estado == ESTADO.OP1
     display.innerHTML+= porcentaje.value;
+    ESTADO.seno = false;
+}
+
+document.getElementById('raiz').onclick =() =>{
+  resultado= Math.sqrt(display.innerHTML);
+  display.innerHTML=resultado
+  
+  
+}
+
+sin.onclick =()=>{
+    display.innerHTML = 'sin ('.concat(display.innerHTML).concat(')')
+    var valor_disp = display.innerHTML;
+    resultado_seno = valor_disp.replace( /(^.*\[|\].*$)/g, '' );
+    resultado_sin= valor_disp * Math.PI/180;
+    resultado = Math.sin(resultado_seno);
+}
+cos.onclick =()=>{
+  display.innerHTML = 'Math.cos ('.concat(display.innerHTML).concat(')')
+  var valor_disp_cos = display.innerHTML;
+  resultado_coseno = valor_disp_cos.replace( ')', '' ).replace('(', '' ).replace('cos','');
+  resultado_coseno= valor_disp_cos* Math.PI/180;
+  resultado = Math.cos(resultado_coseno);
+  alert(valor_disp_cos)
+  //display.innerHTML=Math.cos(display.innerHTML * Math.PI/180 )
+}
+tan.onclick =()=>{
+  display.innerHTML = 'tan('.concat(display.innerHTML).concat(')')
+  var valor_disp = display.innerHTML;
+  resultado_seno = valor_disp.replace( /(^.*\[|\].*$)/g, '' );
+  resultado_sin= valor_disp * Math.PI / 180;
+  resultado = Math.tan(resultado_sin);
 }
 
 //-- Calculos
 igual.onclick = () => {
+  sin.disabled = true;
+  cos.disabled = true;
+  tan.disabled = true;
   if(estado == ESTADO.OP1 ||  estado == ESTADO.OP2){
-    resultado = eval(display.innerHTML.replace('π', 'Math.PI').replace('%','*0.01'));
+    resultado = eval(display.innerHTML.replace('π', 'Math.PI').replace('%','*0.01').replace('^','**').replace('tan','Math.tan').replace('sin','Math.sin'));
     display.innerHTML = resultado
     estado = ESTADO.OP1;
   }
@@ -87,26 +135,43 @@ punto.onclick = (ev) => {
 reset.onclick = () => {
     display.innerHTML = "";
     display_err.innerHTML=""
-    estado = ESTADO.INIT
+    sin.disabled = true;
+    cos.disabled = true;
+    tan.disabled = true;
 }
   
 //-- Borra el ultimo digito(C)
 clear.onclick = () => {
-    display.innerHTML= display.innerHTML.slice(0, -1);
-    display_err.innerHTML="";
+  display.innerHTML= display.innerHTML.slice(0, -1);
+  display_err.innerHTML="";
+  sin.disabled = true;
+  cos.disabled = true;
+  tan.disabled = true;
 }
 
 //-- boton ans
 ans.onclick = () =>{
+  sin.disabled = false;
+  cos.disabled = false;
+  tan.disabled = false;
   if(estado != ESTADO.INIT){
     display.innerHTML += resultado;
-    estado = ESTADO.OP1;
+    //estado = ESTADO.OP1;
   }
 }
-  
+
+
   // mostrar syntax error
-window.onerror = function() {
+  /*window.onerror = function() {
     //alert('Syntax Error');
     display_err.innerHTML="Syntax Error"
     return true;
+}*/
+
+//tabs
+document.getElementById('normal').onclick= () =>{
+    document.getElementById('sci').classList.add('hide')
+}
+document.getElementById('scientific').onclick= () =>{
+  document.getElementById('sci').classList.remove('hide')
 }
