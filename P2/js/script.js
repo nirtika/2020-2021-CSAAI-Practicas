a0ed3c2d220b632e401f1ced973280918311b24e
Nirtika <14869446+nirtika@users.noreply.github.com>
2021-04-02 01:30:18 +0200

    parte scientic mas operaciones

--- P2/js/script.js
+++ P2/js/script.js
@@ -1,13 +1,21 @@
 console.log("Ejecutando JS...");
 //-- Elementos de la interfaz de la calculadora
 const display = document.getElementById("display_res")
-const display_err= document.getElementById("display_err")
+const display_res= document.getElementById("display_err")
 const igual = document.getElementById("igual")
 const reset = document.getElementById("reset")
 const clear = document.getElementById("clear")
 const punto = document.getElementById("punto")
 const ans = document.getElementById("ans")
-const porcentaje = document.getElementById('porcentaje');
+const porcentaje = document.getElementById('porcentaje')
+const factorial = document.getElementById('fact')
+const ln = document.getElementById('ln')
+const log = document.getElementById('log')
+const abs = document.getElementById('abs')
+const pow_2 = document.getElementById('power_2')
+const ex = document.getElementById('ex')
+const camb_signo = document.getElementById('cambiar_signo')
+const inv = document.getElementById('inv')
 // todos los 'digits' que hay en el html
 let digitos = document.getElementsByClassName("digits")
 //-- array con todos los elementos de la clase operacion
@@ -33,9 +41,6 @@ let estado = ESTADO.INIT;
 //-- el array de los digitos
 for (i=0; i<digitos.length; i++){
   digitos[i].onclick = (ev)=> {
-    sin.disabled = false;
-    cos.disabled = false;
-    tan.disabled= false;
     digito(ev.target.value); // ev=evento, ev.target da el valor del boton que ha sido pulsado
   }
 }
@@ -43,9 +48,6 @@ for (i=0; i<digitos.length; i++){
 //-- el array de los operadores: sumar, restar, multiplicar, dividir,
 for (i=0; i<operacion.length; i++){
   operacion[i].onclick = (ev)=> {
-    sin.disabled = false;
-    cos.disabled = false;
-    tan.disabled= false;
     if(estado == ESTADO.OP1){
       display.innerHTML += ev.target.value;
       estado = ESTADO.OPERATION;
@@ -53,11 +55,7 @@ for (i=0; i<operacion.length; i++){
     }
   }
 }
-if(display.innerHTML =="" && display_err.innerHTML==""){
-  sin.disabled = true;
-  cos.disabled = true;
-  tan.disabled = true;
-}
+
 function digito(boton) {
   if(estado == ESTADO.INIT) {
       display.innerHTML = boton;
@@ -71,50 +69,12 @@ function digito(boton) {
     }
     
 }
-porcentaje.onclick =() =>{
-    estado == ESTADO.OP1
-    display.innerHTML+= porcentaje.value;
-    ESTADO.seno = false;
-}
-
-document.getElementById('raiz').onclick =() =>{
-  resultado= Math.sqrt(display.innerHTML);
-  display.innerHTML=resultado
-  
-  
-}
-
-sin.onclick =()=>{
-    display.innerHTML = 'sin ('.concat(display.innerHTML).concat(')')
-    var valor_disp = display.innerHTML;
-    resultado_seno = valor_disp.replace( /(^.*\[|\].*$)/g, '' );
-    resultado_sin= valor_disp * Math.PI/180;
-    resultado = Math.sin(resultado_seno);
-}
-cos.onclick =()=>{
-  display.innerHTML = 'Math.cos ('.concat(display.innerHTML).concat(')')
-  var valor_disp_cos = display.innerHTML;
-  resultado_coseno = valor_disp_cos.replace( ')', '' ).replace('(', '' ).replace('cos','');
-  resultado_coseno= valor_disp_cos* Math.PI/180;
-  resultado = Math.cos(resultado_coseno);
-  alert(valor_disp_cos)
-  //display.innerHTML=Math.cos(display.innerHTML * Math.PI/180 )
-}
-tan.onclick =()=>{
-  display.innerHTML = 'tan('.concat(display.innerHTML).concat(')')
-  var valor_disp = display.innerHTML;
-  resultado_seno = valor_disp.replace( /(^.*\[|\].*$)/g, '' );
-  resultado_sin= valor_disp * Math.PI / 180;
-  resultado = Math.tan(resultado_sin);
-}
 
 //-- Calculos
 igual.onclick = () => {
-  sin.disabled = true;
-  cos.disabled = true;
-  tan.disabled = true;
+  display_res.innerHTML=display.innerHTML
   if(estado == ESTADO.OP1 ||  estado == ESTADO.OP2){
-    resultado = eval(display.innerHTML.replace('π', 'Math.PI').replace('%','*0.01').replace('^','**').replace('tan','Math.tan').replace('sin','Math.sin'));
+    resultado = eval(display.innerHTML.replace('π', '*Math.PI').replace('%','*0.01').replace('^','**'))
     display.innerHTML = resultado
     estado = ESTADO.OP1;
   }
@@ -134,32 +94,65 @@ punto.onclick = (ev) => {
 //-- limpiar (boton AC)
 reset.onclick = () => {
     display.innerHTML = "";
-    display_err.innerHTML=""
-    sin.disabled = true;
-    cos.disabled = true;
-    tan.disabled = true;
+    display_res.innerHTML=""
+    
 }
   
 //-- Borra el ultimo digito(C)
 clear.onclick = () => {
   display.innerHTML= display.innerHTML.slice(0, -1);
-  display_err.innerHTML="";
-  sin.disabled = true;
-  cos.disabled = true;
-  tan.disabled = true;
+  display_res.innerHTML="";
+  
 }
 
 //-- boton ans
 ans.onclick = () =>{
-  sin.disabled = false;
-  cos.disabled = false;
-  tan.disabled = false;
   if(estado != ESTADO.INIT){
     display.innerHTML += resultado;
     //estado = ESTADO.OP1;
   }
 }
-const factorial = document.getElementById('fact')
+
+//porcentaje
+porcentaje.onclick =() =>{
+  estado == ESTADO.OP1
+  display.innerHTML+= porcentaje.value;
+}
+
+//raiz cuadrada
+document.getElementById('raiz').onclick =() =>{  
+  display_res.innerHTML= '&Sqrt;'.concat(display.innerHTML)
+  resultado= Math.sqrt(display.innerHTML);
+  display.innerHTML=resultado
+}
+
+//seno
+sin.onclick =()=>{
+  display_res.innerHTML = 'sin ('.concat(display.innerHTML).concat(')')
+  var valor_disp = eval(display.innerHTML.replace('π', 'Math.PI'))
+  resultado_sin= valor_disp * Math.PI/180
+  resultado = Math.sin(resultado_sin)
+  display.innerHTML= resultado
+}
+
+//coseno
+cos.onclick =()=>{
+  display_res.innerHTML = 'cos ('.concat(display.innerHTML).concat(')')
+  var valor_disp_cos = eval(display.innerHTML.replace('π', 'Math.PI'))
+  resultado_coseno= valor_disp_cos* Math.PI/180
+  resultado = Math.cos(resultado_coseno)
+  display.innerHTML= resultado
+}
+
+//tan
+tan.onclick =()=>{
+  display_res.innerHTML = 'tan('.concat(display.innerHTML).concat(')')
+  var valor_disp = eval(display.innerHTML.replace('π', 'Math.PI'))
+  resultado_tan= valor_disp * Math.PI / 180;
+  resultado = Math.tan(resultado_tan);
+  display.innerHTML= resultado
+}
+
 //factorial
 factorial.onclick =()=>{
   var dato = display.innerHTML;
@@ -169,45 +162,74 @@ factorial.onclick =()=>{
       resultado = x
       display.innerHTML= resultado
   }
+  display_res.innerHTML= dato.concat('!')
 }
 
 //ln
-const ln = document.getElementById('ln')
 ln.onclick =()=>{
   resultado = Math.log(display.innerHTML)
   display.innerHTML= resultado
 }
 
 //log
-const log = document.getElementById('log')
 log.onclick =()=>{
   resultado = Math.log10(display.innerHTML)
   display.innerHTML= resultado
 }
 
 // abs
-const abs = document.getElementById('abs')
 abs.onclick =()=>{
   resultado = Math.abs(display.innerHTML)
   display.innerHTML= resultado
 }
-// abs
-const pow_2 = document.getElementById('power_2')
+
+// x^2
 pow_2.onclick =()=>{
+  display_res.innerHTML= display.innerHTML.concat('<sup>2</sup>')
   resultado = Math.pow(display.innerHTML,2)
   display.innerHTML= resultado
 }
-  // mostrar syntax error
-  /*window.onerror = function() {
-    //alert('Syntax Error');
-    display_err.innerHTML="Syntax Error"
+
+//e^x
+ex.onclick =()=>{
+  display_res.innerHTML= ('e<sup>').concat(display.innerHTML).concat('</sup>')
+  resultado = Math.exp(display.innerHTML)
+  display.innerHTML= resultado
+}
+
+//cambio de signo
+camb_signo.onclick =()=>{
+  var num = Math.sign(display.innerHTML)
+  if(num ==1){
+    display.innerHTML= - display.innerHTML
+  }else{
+    display.innerHTML= display.innerHTML*-1
+  }
+}
+
+//inv
+inv.onclick =()=>{
+  var num = display.innerHTML
+  display_res.innerHTML= num.concat('<sup>-1</sup>')
+  resultado = (1/num)
+  display.innerHTML= resultado
+}
+
+//mostrar syntax error
+window.onerror = function() {
+    alert('Syntax Error');
+    display_res.innerHTML="Syntax Error"
     return true;
-}*/
+}
 
 //tabs
 document.getElementById('normal').onclick= () =>{
     document.getElementById('sci').classList.add('hide')
+    reset.onclick()
+    estado= ESTADO.INIT
 }
 document.getElementById('scientific').onclick= () =>{
   document.getElementById('sci').classList.remove('hide')
+  reset.onclick()  
+  estado= ESTADO.INIT
 }
