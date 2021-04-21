16f3004e2c11b11b4c4cc3065f075a7254d00396
Nirtika <14869446+nirtika@users.noreply.github.com>
2021-04-21 22:25:44 +0200

    dibujar elementos

--- P3/js/script.js
+++ P3/js/script.js
@@ -2,4 +2,25 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
 
+// dibujar palo
+ctx.beginPath();
+ctx.rect(2, 140, 50, 8);
+ctx.fillStyle = "#FFFFFF";
+ctx.fill();
+ctx.closePath();
+
+// dibujar ladrillo
+ctx.beginPath();
+ctx.rect(2, 14, 50, 5);
+ctx.fillStyle = "blue";
+ctx.fill();
+ctx.closePath();
+
+//dibujar bola
+ctx.beginPath();
+ctx.arc(150, 80, 8, 0, Math.PI*2);
+ctx.fillStyle = "red";
+ctx.fill();
+ctx.closePath();
+
 
