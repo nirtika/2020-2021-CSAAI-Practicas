42abfe3e7d4234c36c67b2327090599b2fc5b6af
Nirtika <14869446+nirtika@users.noreply.github.com>
2021-05-06 18:15:08 +0200

    duplicado borrado

--- P4/L11/Ejercicio3.js
+++ P4/L11/Ejercicio3.js
@@ -34,23 +34,6 @@ img.onload = function () {
   console.log("Imagen lista...");
 };
 
-//-- Función de retrollamada de imagen cargada
-//-- La imagen no se carga instantaneamente, sino que
-//-- lleva un tiempo. Sólo podemos acceder a ella una vez
-//-- que esté totalmente cargada
-img.onload = function () {
-
-  //-- Se establece como tamaño del canvas el mismo
-  //-- que el de la imagen original
-  canvas.width = img.width;
-  canvas.height = img.height;
-
-  //-- Situar la imagen original en el canvas
-  //-- No se han hecho manipulaciones todavia
-  ctx.drawImage(img, 0,0);
-
-  console.log("Imagen lista...");
-};
 
 color.onclick = () =>{
     deslizadorrojo.value = 255;
