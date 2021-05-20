b3a3c5e14b17fe30f84c59e86a0bae8df983c29e
Nirtika <14869446+nirtika@users.noreply.github.com>
2021-05-20 21:00:02 +0200

    empezando....

---
+++ P5/js/script.js
@@ -0,0 +1,76 @@
+//----- Obtener elemento de video y configurarlo
+let directo = document.getElementById("directo");
+const btn_test = document.getElementById("btn_test");
+const btn_src_on = document.getElementById("btn_src_on");
+const video1 = document.getElementById("video1");
+const btn_video1 = document.getElementById("btn_video1");
+const video2 = document.getElementById("video2");
+const btn_video2 = document.getElementById("btn_video2");
+const video3 = document.getElementById("video3");
+const btn_video3 = document.getElementById("btn_video3");
+const img_stand = document.getElementById("img_test");
+const btn_video4 = document.getElementById("btn_video4");
+
+
+//-- Establecer las dimensiones de los vídeos
+directo.width=420;
+directo.height=200;
+video1.width=200;  
+video1.height=100;
+video2.width=200;  
+video2.height=100;
+video3.width=200;  
+video3.height=100;
+img_stand.width=200;  
+img_stand.height=100;
+
+//-- Imagen de Test usada
+const TEST_IMAGE_URL = "img/test.jpg";
+const TEST_IMAGE_URL2 = "img/standby.png";
+
+//-- Imagen estática a mostrar cuando el video no
+//-- ha arrancado
+directo.poster = TEST_IMAGE_URL;
+video1.poster = TEST_IMAGE_URL;
+video2.poster = TEST_IMAGE_URL;
+video3.poster = TEST_IMAGE_URL;
+img_stand.src = TEST_IMAGE_URL;
+
+//-- Boton de FUENTES-ON
+btn_src_on.onclick = () => {
+ 
+  //-- Establecer la fuente de la cámara 1
+  video1.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4";
+  video2.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4";
+  video3.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4";
+  img_stand.src=TEST_IMAGE_URL2;
+
+  //-- Reprodeucimos un vídeo, desde el comienzo
+  video1.currentTime = 0;
+  video1.play();
+  video2.currentTime = 0;
+  video2.play();
+  video3.currentTime = 0;
+  video3.play();
+  
+
+  //-- Y en silencio...
+  video1.muted;
+
+  //-- En la emisión en directo ponemos la imagen de prueba
+  directo.poster = TEST_IMAGE_URL;
+};
+
+//-- Botón de Test
+btn_test.onclick = () => {
+    directo.poster = TEST_IMAGE_URL;
+    directo.src = null;
+};
+
+//-- Botón de Selección de la cámara 1
+btn_video1.onclick = () => {
+    directo.src = video1.src;
+    directo.currentTime = video1.currentTime;
+    directo.play();
+    directo.poster=null;
+};
